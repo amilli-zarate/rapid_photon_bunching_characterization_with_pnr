65e8120dc0dcf0a8d1f8f671d530df2f039c2dc9
amilli-zarate <amilli.zarate@outlook.com>
2026-06-14 18:45:41 -0600

    Added code to remove generator scripts afterwards usage.

--- data_generation/complete_distributions/compute_complete_distributions.sh
+++ data_generation/complete_distributions/compute_complete_distributions.sh
@@ -4,8 +4,14 @@ cd data_generation/complete_distributions/
 
 python generator_of_generators.py
 
-for script in $(ls | grep -E '^[0-9]+_generator\.py$'); do
+scripts=$(printf '%s\n' *.py | grep -E '^[0-9]+_generator\.py$')
+
+for script in $scripts; do
     python "$script" &
 done
 
 wait
+
+for script in $scripts; do
+    rm "$script"
+done
